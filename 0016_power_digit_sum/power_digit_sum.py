3debf13c3e41d00eada88d23f47c91608384ae1d
Vineet John <vinjohn10@gmail.com>
2016-05-30 15:40:44 +0530

    Added solution for problem 16

--- 0016_power_digit_sum/power_digit_sum.py
+++ 0016_power_digit_sum/power_digit_sum.py
@@ -1,4 +1,12 @@
-import math
 
-number = math.pow(2, 1000)
-print "number", number
+number = 2 ** 1000
+# print "number", number
+
+summation = 0
+while number >= 10:
+    summation += number % 10
+    number = number / 10
+
+summation += number % 10
+
+print summation
